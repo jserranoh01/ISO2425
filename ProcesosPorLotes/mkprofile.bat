c04ef506e5ef992d57779d8c917b9e89eecf5761
jserranoh01 <jaserranoh01@educarex.es>
2024-11-18 09:51:10 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,10 @@
+@echo off
+rem Scrip mkprofile
+rem Autor Jose Antonio Serrano Hernandez
+rem 13-11-2024	
+mkdir %userprofile%/Asir %userprofile%/Dam %userprofile%/Smr %userprofile%/Daw %userprofile%/Ceti
+mkdir %userprofile%/Asir/docs %userprofile%/Asir/prac %userprofile%/Asir/exam
+mkdir %userprofile%/Dam/docs %userprofile%/Dam/prac %userprofile%/Dam/exam
+mkdir %userprofile%/Smr/docs %userprofile%/Smr/prac %userprofile%/Smr/exam
+mkdir %userprofile%/Daw/docs %userprofile%/Daw/prac %userprofile%/Daw/exam
+mkdir %userprofile%/Ceti/docs %userprofile%/Ceti/prac %userprofile%/Ceti/exam
