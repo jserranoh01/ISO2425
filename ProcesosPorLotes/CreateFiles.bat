addfd25859a76fe29f949b408c912d94221f49df
jserranoh01 <jaserranoh01@educarex.es>
2024-11-20 10:54:40 +0100

    Add files via upload

---
+++ ProcesosPorLotes/CreateFiles.bat
@@ -0,0 +1,17 @@
+@echo off
+:: CreateFiles.bat
+:: Jose Antonio Serrano Hernandez
+:: Fecha 20/11/2024
+set /p op="Pulsa 1 para texto Pulsa 2 Para archivo .bat "
+if %op% == 1 goto op1
+if %op% == 2 goto op2
+:op1
+set /p name="nombre de texto "
+echo > %name%.text
+pause
+exit
+:op2
+set /p arc="nombre de archivo "
+echo >%arc%.bat
+pause
+exit
